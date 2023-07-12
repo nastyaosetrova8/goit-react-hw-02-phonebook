85d0dad7634741b46518cc052f684b08ce6fb575
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-07-12 19:18:46 +0200

    Add HW-02-phonebook

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -12,7 +12,7 @@ const Section = ({title, children}) => {
     }
     
     Section.propTypes = {
-        title: PropTypes.string.isRequired,
+        title: PropTypes.string,
         children: PropTypes.node.isRequired,
     }
 
